736c560370293efa2d3addafa82aa0b50abfc837
mrvae87-star <mr.v.a.e87@gmail.com>
2026-07-31 18:05:19 +0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,15 +1,27 @@
-import { initializeApp } from 
-"https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+// Firebase подключение
 
-import { getFirestore } from 
-"https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+
+// Настройки твоего проекта Firebase
 
 const firebaseConfig = {
-  ВСТАВЬ_СЮДА_СВОЙ_КОД_FIREBASE
+  apiKey: "AIzaSyAf8UcXSI1VpTe_qytCxeomUt_ENjKgoZY",
+  authDomain: "pvz-schedule2.firebaseapp.com",
+  projectId: "pvz-schedule2",
+  storageBucket: "pvz-schedule2.firebasestorage.app",
+  messagingSenderId: "680318009283",
+  appId: "1:680318009283:web:f5f8dea852b505f89e8776"
 };
 
 
+// Запуск Firebase
+
 const app = initializeApp(firebaseConfig);
+
+
+// Подключение Firestore
 
 export const db = getFirestore(app);
